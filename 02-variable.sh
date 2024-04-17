28feab44dfdfe57b011fcc2246aeffb0e3ccb533
kavya <kavyapothuguntla23@gmail.com>
2024-04-17 18:06:18 -0400

    hello script

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,1 +1,6 @@
-this again sample script
+#!/bib/bash
+
+echo "sudiksha:: Hello kavya, how are you?"
+echo "kavya":: Hi sudiksha, iam fine. how are you?"
+echo "sudiksha:: I am fine too. how is your work?"
+echo "kavya:: Not bad. I am thinking to upgrade to devops"
