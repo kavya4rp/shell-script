1c9e79e69006f2819c3d10171df32025d4f3599d
kavya <kavyapothuguntla23@gmail.com>
2024-04-17 19:03:17 -0400

    hello script

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,6 +1,6 @@
 #!/bib/bash
 
 echo "sudiksha:: Hello kavya, how are you?"
-echo "kavya":: Hi sudiksha, iam fine. how are you?"
+echo "kavya :: Hi sudiksha, iam fine how are you?"
 echo "sudiksha:: I am fine too. how is your work?"
-echo "kavya:: Not bad. I am thinking to upgrade to devops"
+echo "kavya:: not bad. I am thinking to upgrade to Devops"
--- 03-variable.sh
+++ 03-variable.sh
@@ -4,6 +4,6 @@ PERSON1=sudiksha #no space between = and value
 PERSON2=kavya
 
 echo "$PERSON1:: Hello $PERSON2, how are you?"
-echo "$PERSON2":: Hi $PERSON1, iam fine. how are you?"
+echo "$PERSON2:: Hi $PERSON1, iam fine. how are you?"
 echo "$PERSON1:: I am fine too. how is your work?"
 echo "$PERSON2:: Not bad. I am thinking to upgrade to devops"
