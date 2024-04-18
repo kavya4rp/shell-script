03af5c6a96d89a89196663ed771e09d4898b1c56
kavya <kavyapothuguntla23@gmail.com>
2024-04-18 15:36:41 -0400

    hello script

---
+++ 05-variable.sh
@@ -0,0 +1,12 @@
+#!/bib/bash
+
+#declaring of varible
+
+PERSON1=$rajesh
+PERSON2=$divya
+#referring varible
+
+echo "$PERSON1:: Hello $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, iam fine. how are you?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: Not bad. I am thinking to upgrade to devops"
