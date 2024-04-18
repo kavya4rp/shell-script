39dbaca5c817c3babcc06be9b72ee79507e88d5d
kavya <kavyapothuguntla23@gmail.com>
2024-04-18 16:10:49 -0400

    hello script

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,12 +1,9 @@
-#!/bib/bash
+#!/bin/bash
 
-#declaring of varible
+echo "please enter username::"
 
-PERSON1=$rajesh
-PERSON2=$divya
-#referring varible
+read USERNAME #here USERNAME is variable
 
-echo "$PERSON1:: Hello $PERSON2, how are you?"
-echo "$PERSON2:: Hi $PERSON1, iam fine. how are you?"
-echo "$PERSON1:: I am fine too. how is your work?"
-echo "$PERSON2:: Not bad. I am thinking to upgrade to devops"
+echo "please enter password::"
+read PASSWORD
+echo "username is: $USERNAME, password is: $PASSWORD"
