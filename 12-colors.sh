c63453646a9bb109163c3c7e9712cfde419aa85a
kavya <kavyapothuguntla23@gmail.com>
2024-04-21 11:21:58 -0400

    functions

---
+++ 12-colors.sh
@@ -0,0 +1,33 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...$R FAILURE $N"
+        exit 1
+    else 
+         echo "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then   
+     echo "Please run this script with root acess."
+     exit 1 # manually exit if error comes.
+else
+    echo "you are super user."
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MYSQL"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing GIT"
