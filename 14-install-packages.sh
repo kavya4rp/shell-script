ebd669afd565751fa267a9cb0de7c85bbf89ecd4
kavya <kavyapothuguntla23@gmail.com>
2024-04-21 14:00:02 -0400

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -4,6 +4,11 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 VALIDATE(){
     if [ $1 -ne 0]
     then 
