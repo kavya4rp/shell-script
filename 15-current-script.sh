9cc56cea4a53cf660ec01c1843633de310f12bfd
kavya <kavyapothuguntla23@gmail.com>
2024-04-23 17:48:16 -0400

    functions

---
+++ 15-current-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COURSE="DevOps from Current script"
+
+echo "Before calling other script, course: $COURCE"
+echo  "Process Id of current shell script: $$"
+
+#./16-other-script.sh
+
+echo "After calling other script, course: $COURSE"
+echo "Process ID of current shell script: $$"
